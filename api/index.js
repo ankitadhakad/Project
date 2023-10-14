62d1d3bac3d3c09e0990e8ac0fd99321f7b4bd7e
Ankita <ankita.dhakad03@gmail.com>
2023-10-14 11:20:01 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose
+.connect(process.env.MONGO)//MONGO=environment variable so that everybody cant seee password
+.then (() => {
+    console.log('Connected to MONGODB!');
+})//to check wether we are connected to database//
+.catch((err) => {
+    console.log(err);
+})
+
 
 const app=express();
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
