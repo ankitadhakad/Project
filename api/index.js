f622cf515362fa36b8fff52d9fedde24e9b5c84a
Ankita <ankita.dhakad03@gmail.com>
2023-10-15 10:06:35 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose
 .connect(process.env.MONGO)//MONGO=environment variable so that everybody cant seee password
@@ -18,3 +19,10 @@ const app=express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+//path:api/index.js
+// app.get('/',(req,res) =>
+// {
+//     res.send('Hello World!');
+// });
+app.use('/api/user',userRouter);
