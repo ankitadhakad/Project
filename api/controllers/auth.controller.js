86f1931841d5b82c2ceb855c16f9f4e2ade81151
Ankita <ankita.dhakad03@gmail.com>
2023-10-16 23:33:36 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req,res,next) => {//to use await we have to make funciton async
     const {username,email,password} = req.body;
 
@@ -21,3 +23,23 @@ export const signup = async (req,res,next) => {//to use await we have to make fu
    
 
 };
+
+
+export const signin = async (req,res,next) => {
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser)
+        return next(errorHandler(404,'User not found!'));
+        const validPassword = bcryptjs.compareSync(password,validUser.password);//need to compare decrypted password as encrypted password is stored in data base therefore ee need to decrypt them
+        if(!validPassword)
+        return next(errorHandler(401,'Wrong Credentials!'));
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const {password:pass, ...rest} = validUser._doc;
+        res.cookie('access_token',token,{httpOnly: true}).status(200).json(rest);
+
+    }catch(error)
+    {
+        next(error);
+    }
+}
