6db5a7b02f6b81d0cf49d8e69c0e62aacc840d39
Ankita <ankita.dhakad03@gmail.com>
2023-10-21 14:34:20 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,25 +1,118 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {useSelector} from 'react-redux';
+import {useRef} from 'react';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import { app } from '../firebase';
+
 
 export default function Profile() {
+ 
+  const fileRef= useRef(null);
   const {currentUser} = useSelector((state) => state.user); 
+  const [file,setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError,setFileUploadError] = useState(false);
+  const [formData,setFromData] = useState({});
+// console.log(formData);
+// console.log(filePerc);
+// console.log(fileUploadError);
+  // console.log(filePerc);
+  // console.log(file);
+
+
+  useEffect(() => {
+    if(file)
+    {
+      handleFileUpload(file);
+    }
+  },[file]);
+
+const handleFileUpload =(file) => {
+     const storage = getStorage(app);
+     const fileName = new Date().getTime() + file.name;
+     const StorageRef = ref(storage,fileName);
+     const uploadTask = uploadBytesResumable(StorageRef,file);
+
+     uploadTask.on('state_changed',
+     (snapshot) => {
+      const progress = (snapshot.bytesTransferred / 
+      snapshot.totalBytes)*100;
+      setFilePerc(Math.round(progress));
+      // console.log('upload is'+progress+'% done');
+     },
+     (error) => {
+      setFileUploadError(true);
+     },
+     ()=> {
+      getDownloadURL(uploadTask.snapshot.ref)
+      .then((downloadURL) => {
+              setFromData({...formData,avatar:downloadURL});
+      })
+     }
+     );
+
+}
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
-        src={currentUser.avatar} alt=''/>
+        <input onChange={(e)=>setFile(e.target.files[0])}
+        type = "file" ref = {fileRef} hidden accept='image/*' />
+        
+        <img 
+        onClick = {() => fileRef.current.click()} 
 
-        <input type="text" placeholder='username' id='username'
+        className = 'rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        src = {formData.avatar || currentUser.avatar} alt=''/>
+      
+       <p className='text-sm self-center'>
+        {fileUploadError ?
+        (
+        <span className='text-red-700'>
+         Error Image upload
+         (image must be less than 2 MB)
+        </span>
+        ) :
+        filePerc >0 && filePerc < 100 ? 
+        (
+          <span className='text-slate-700'>
+            {`Uploading ${filePerc}%`}
+          </span>
+          ):
+          
+          filePerc === 100 ? 
+          (
+            <span className='text-green-700'>
+              Image  successfully uploaded!
+            </span>
+            ) :("")
+          
+        
+      }
+       </p>
+
+        <input 
+        type="text" 
+        placeholder='username' 
+        id='username'
         className='border p-3 rounded-lg'/>
 
-          <input type="email" placeholder='email' id='email'
-        className='border p-3 rounded-lg'/>
+          <input 
+          type="email" 
+          placeholder='email' 
+          id='email'
+          className='border p-3 rounded-lg'/>
 
-          <input type="password" placeholder='password' id='password'
-        className='border p-3 rounded-lg'/>
+          <input 
+          type="password" 
+          placeholder='password' 
+          id='password'
+          className='border p-3 rounded-lg'/>
 
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95
+        <button 
+        className='bg-slate-700 
+        text-white rounded-lg p-3 uppercase hover:opacity-95
         disabled:opacity-80'>
           update
         </button>
@@ -35,3 +128,17 @@ export default function Profile() {
       </div>
   )
 }
+
+
+
+//firebase storage
+// service firebase.storage {
+//   match /b/{bucket}/o {
+//     match /{allPaths=**} {
+//       allow read;
+//       allow write: if 
+//       request.resource.size <2*1024*1024 && 
+//       request.resource.contentType.matches('image/.*')
+//     }
+//   }
+// }
