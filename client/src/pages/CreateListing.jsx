f4bfaba4aa1c4c6722ba1cd43f9eb3f725ea1500
Ankita <ankita.dhakad03@gmail.com>
2023-12-29 11:35:32 +0530

    converting prices

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -257,7 +257,7 @@ export default function CreateListing() {
                     className='p-3 border border-gray-300 rounded-lg'
                     type = "number"
                     id = 'regularPrice'
-                    min = '50'
+                    min = '50000'
                     max = '10000000'
                     required
                     onChange={handleChange}
@@ -266,7 +266,7 @@ export default function CreateListing() {
                      <div className='flex flex-col items-center'>
                         <p>Regular price</p>
                         {formData.type === 'rent' && (
-                  <span className='text-xs'>($ / month)</span>
+                  <span className='text-xs'>(₹ / month)</span>
                 )}
                      </div>
                      
@@ -287,7 +287,7 @@ export default function CreateListing() {
 
                      <p>Discounted price</p>
                      {formData.type === 'rent' && (
-                    <span className='text-xs'>($ / month)</span>
+                    <span className='text-xs'>(₹ / month)</span>
                   )}
                      </div>
                 </div>
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -284,7 +284,7 @@ export default function UpdateListing() {
                      <div className='flex flex-col items-center'>
                         <p>Regular price</p>
                         {formData.type === 'rent' && (
-                  <span className='text-xs'>($ / month)</span>
+                  <span className='text-xs'>(₹ / month)</span>
                 )}
                      </div>
                      
@@ -305,7 +305,7 @@ export default function UpdateListing() {
 
                      <p>Discounted price</p>
                      {formData.type === 'rent' && (
-                    <span className='text-xs'>($ / month)</span>
+                    <span className='text-xs'>(₹  / month)</span>
                   )}
                      </div>
                 </div>
